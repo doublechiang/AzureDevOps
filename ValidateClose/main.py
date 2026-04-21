98549432e82463291c17d0b0c744cc614f0ae283
Chunyu Chiang <chun-yu.chiang@quantatw.com>
2026-04-21 12:12:42 +0800

    debug workitem state change

--- ValidateClose/main.py
+++ ValidateClose/main.py
@@ -129,6 +129,7 @@ def check_issue_status():
 
             for m in mentions:
                 guid = get_guid_by_email(m, auth)
+                print(f"DEBUG: guid = {guid}", flush=True)
                 display_name = m.split('@')[0]
                 if guid is not None:
                     tag = f'<a href="mailto:{m}" data-vss-mention="version:2.0,guid:{guid}">@{display_name}</a>'
